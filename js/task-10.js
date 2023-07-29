2a1e2458f8ecfc61396080d3abba2d8507ffc5dd
MykhailoYavorskyi <my70035@gmail.com>
2023-07-29 09:13:43 +0200

    10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,20 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controlsRef = document.querySelector("#controls");
+const boxRef = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  let divMarkup = "";
+  for (let i = 1, j = 30; i <= amount; i += 1, j += 10) {
+    divMarkup += `<div style="height: ${j}px; width: ${j}px; background-color: ${getRandomHexColor()}"></div>`;
+  }
+  boxRef.innerHTML = divMarkup;
+}
+function destroyBoxes() {
+  boxRef.innerHTML = "";
+}
+
+controlsRef.firstElementChild.nextElementSibling.addEventListener("click", createBoxes);
+controlsRef.lastElementChild.addEventListener("click", destroyBoxes);
